796bfdbc22088abe96a2530d14f139a77e2af886
Momen-Code <59899828+Momen-Code@users.noreply.github.com>
2020-06-20 20:27:48 +0200

    Add files via upload

---
+++ Assets/js/index.js
@@ -0,0 +1,18 @@
+
+document.querySelector(".h-burger").onclick = function() {
+    document.querySelector(".menu-wrap").classList.toggle("open");
+};
+
+
+
+var typed = new Typed('.element', {
+    strings: ["Fron-end Developer", "Full-Stack Web Developer"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    showCursor: false,
+    loop: false,
+    loopCount: Infinity
+  });
+
+
+
